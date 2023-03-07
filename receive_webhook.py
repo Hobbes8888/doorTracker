9f5aba7df5dc34ab82fb445764fc7ed5aa9af161
Hobbes8888 <97311243+Hobbes8888@users.noreply.github.com>
2023-03-06 19:12:26 -0700

    setting html to print request json

--- receive_webhook.py
+++ receive_webhook.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, abort
+import json
 
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    if request.method == 'POST':
-        html = f"<h1>Deployed with Zeet!!</h1>"
-        print(request.json)
-    return html, request.json
+    html = "<h1>Webhook Received</h1>"
+    html += "<p>Request JSON:</p>"
+    html += "<pre>" + json.dumps(request.json, indent=4) + "</pre>"
+    return html
 
 
 
