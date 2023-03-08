5002ddaa1e1c273a5e29dfc9edfc7bf624444b89
Hobbes8888 <97311243+Hobbes8888@users.noreply.github.com>
2023-03-08 13:38:38 -0700

    get rid of json stuff and just back to html post

--- receive_webhook.py
+++ receive_webhook.py
@@ -1,13 +1,11 @@
 from flask import Flask, request, abort
-import json
+# import json
 
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     html = "<h1>Webhook Received</h1>"
-    html += "<p>Request JSON:</p>"
-    html += "<pre>" + json.dumps(request.json, indent=4) + "</pre>"
     return html
 
 
